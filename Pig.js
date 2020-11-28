8f0237a0a4b572fca3c4b3815444cd48b681d78a
lilspidey0k <72351914+lilspidey0k@users.noreply.github.com>
2020-11-28 14:59:15 -0500

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,11 @@
+class Pig extends Baseclass{
+    constructor(x,y){
+    this.body=Bodies.rectangle(x,y,50,50,rest);
+    super(x,y,50,50)
+ }
+    this.image=loadImage("sprites/enemy.png");
+    super.display();
+    
+    
+    
+}
